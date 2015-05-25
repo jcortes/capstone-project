6ed1966a7b264db7bceb1afa89671b7200a42659
jcortes <jacortesmahmud@gmail.com>
2015-05-25 14:52:07 -0500

    test

--- src/server/api/user/user.controller.js
+++ src/server/api/user/user.controller.js
@@ -32,7 +32,7 @@ exports.show = function(req, res) {
 
 // Get a single user by SoundCloud id
 exports.showById = function(req, res) {
-  User.findOne({ id: req.params.id }, function (err, user) {
+  User.findOne({ id: parseInt(req.params.id) }, function (err, user) {
     if(err) { return handleError(res, err); }
     if(!user) { return res.send(404); }
     return res.json(user);
