e7f9d97c667a587532899a5cec91a4fda1ec87ec
jcortes <jacortesmahmud@gmail.com>
2015-05-25 14:27:26 -0500

    test

--- src/client/app/core/core.user.service.js
+++ src/client/app/core/core.user.service.js
@@ -19,10 +19,7 @@
     function userById($http) {
         return function(id) {
             console.log(id);
-            return $http.get('/api/users/' + id)
-            .success(function(response){
-                console.log(response);
-            });
+            return $http.get('/api/users/' + id);
         };
     }
     
@@ -40,27 +37,27 @@
         return function(){
             SC.get('/me', function(me) {
                 
+                var user = null;
                 userById(me.id).then(function(respose) {
                     console.log(respose.data);
-                    var user = respose.data;
-                    if(user === 'undefined'){
-                        user = {
-                            id: me.id,
-                            username: me.username,
-                            uri: me.uri,
-                            first_name: me.first_name,
-                            last_name: me.last_name,
-                            full_name: me.full_name,
-                            avatar_url: me.avatar_url
-                        };
-                        createUser(user).then(function(data){
-                            console.log('User created in database');
-                        });
-                    } else {
-                        console.log('User exists in database');
-                    }
+                    user = respose.data;
+                    console.log('User exists in database');
+                    
+                }).error(function(status){
+                    console.log(status);
+                    user = {
+                        id: me.id,
+                        username: me.username,
+                        uri: me.uri,
+                        first_name: me.first_name,
+                        last_name: me.last_name,
+                        full_name: me.full_name,
+                        avatar_url: me.avatar_url
+                    };
+                    createUser(user).then(function(data){
+                        console.log('User created in database');
+                    });
                 });
-                console.log('after');
                 
 //                listUsers.then(function(response) {
 //                    var user = response.data.filter(function(u){ return u.id === me.id; })[0];
