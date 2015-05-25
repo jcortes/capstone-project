9e3e1d46f3436dbfe3a9d8170e04d02d02566e85
jcortes <jacortesmahmud@gmail.com>
2015-05-25 08:21:00 -0500

    production env

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+process.env.NODE_ENV = 'production';
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Set default mongo uri to production
